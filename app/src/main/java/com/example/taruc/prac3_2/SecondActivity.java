d4005d05180e468b116d6d488b5bd13aa3acf774
lch <lee_chnhn@hotmail.com>
2017-06-13 09:49:31 +0800

    Initial

--- app/src/main/java/com/example/taruc/prac3_2/SecondActivity.java
+++ app/src/main/java/com/example/taruc/prac3_2/SecondActivity.java
@@ -1,13 +1,27 @@
 package com.example.taruc.prac3_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
-
+     private EditText editTextMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        editTextMessage = (EditText)findViewById(R.id.editTextMessage);
+    }
+
+    public void closeSecond(View v){
+        String msg = editTextMessage.getText().toString();
+        Intent intentMessage = new Intent();
+        intentMessage.putExtra("MESSAGE",msg);
+
+        setResult(RESULT_OK,intentMessage);
+
+        finish();
     }
 }
