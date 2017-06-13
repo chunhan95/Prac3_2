fa012700e119ac1aa9a8690f84e6693cede8fe73
lch <lee_chnhn@hotmail.com>
2017-06-13 09:33:53 +0800

    Initial

--- app/src/main/java/com/example/taruc/prac3_2/MainActivity.java
+++ app/src/main/java/com/example/taruc/prac3_2/MainActivity.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int GET_MESSAGE = 1;
+    private static final int GET_PICTURE = 2;
+    private TextView textViewMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewMessage = (TextView)findViewById(R.id.textViewMessage);
     }
 
     public void showGet(View v){
@@ -19,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent,GET_MESSAGE);
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GET_MESSAGE){
+            if(resultCode == RESULT_OK) {
+                textViewMessage.setText(data.getStringExtra("MESSAGE"));
+            }
+        }else if(requestCode == GET_PICTURE){
+            //Display image
+        }
+    }
 }
